c3f6d16e9648fed268785b81cf43cdde29885a2a
/34ZY <55347142+34zY@users.noreply.github.com>
2023-06-05 11:46:47 +0200

    Update bash.asm

--- bash.asm
+++ bash.asm
@@ -14,9 +14,10 @@ _start:
 	xor rdx, rdx
 	push rax
 
-	push 0x2f2f6269
-	push 0x6e2f6261
-	push 0x73680000
+	push 0x2f2f0000
+	push 0x2f6e6962 
+	push 0x68736162 
+	
 	mov rbx, rsp
 	mov al, 0xb ;; 0xb execve
-	syscall ;; int 0x80
+	int 0x80
